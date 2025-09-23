4293c4639ae52e7d65acd1b3c8c7b493be6542b7
root <root@unique-tuatara-of-devotion.us-east1-a.compute.internal>
2025-09-23 06:54:16 +0000

    latest

---
+++ auto_red_team/NEW_run_medium_workload.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+CUDA_VISIBLE_DEVICES=1 python grok_dspy_jailbreak_harm_est.py --setting toxic_many_samples --out_dir NEW_RESULTS_NEW_PROPOSER --workload medium --seed 42 &
+sleep 30
+CUDA_VISIBLE_DEVICES=3 python grok_dspy_jailbreak_harm_est.py --setting baseline_many_samples --out_dir NEW_RESULTS_NEW_PROPOSER --workload medium --seed 42 &
